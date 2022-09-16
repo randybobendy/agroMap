c24c96c485b7c482275dd012ac9436ab89d9b22a
Paweł Sadkowski <psadkowski@qzsolutions.pl>
2022-09-16 15:23:11 +0200

    Dd

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import folium 
+from folium import features
 import json
 json_path = "/home/psadkowski/Dokumenty/Workspace/Projekty/fast_task/osadkowski_json/lokalizacje.json"
 m = folium.Map(location=[19.22882080078125,
@@ -8,13 +9,16 @@ tooltip = 'Dowiedz się więcej'
 # make markers
 f = open(json_path)
 json_data = json.load(f)
+harvIcon = folium.features.CustomIcon('/home/psadkowski/Dokumenty/Workspace/Projekty/fast_task/icon/harv.png', icon_size=(20,20))
 for single_data in json_data["features"]:
     second_coor, first_coor = (single_data["geometry"]["coordinates"])  
     # TODO dodać nazwy punktów
     place_name = single_data["properties"]["name"]
     description = single_data["properties"]["description"]
+    harvIcon = folium.features.CustomIcon('/home/psadkowski/Dokumenty/Workspace/Projekty/fast_task/icon/harv.png', icon_size=(30,30))
     folium.Marker([first_coor,
               second_coor],
               popup=f'<strong> {place_name}, Opis: {description}  </strong>',
-              tooltip=tooltip).add_to(m)
+              icon=harvIcon,
+              tooltip=tooltip).add_to(m) 
 m.save('map.html')
